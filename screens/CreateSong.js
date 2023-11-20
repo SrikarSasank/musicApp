d09d28632a519b722ec488d3fb66b764c9d0b4de
SrikarSasank <103879271+SrikarSasank@users.noreply.github.com>
2023-11-20 21:55:39 +0530

    Add files via upload

---
+++ screens/CreateSong.js
@@ -0,0 +1,327 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  TextInput,
+  Dimensions,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import DropDownPicker from "react-native-dropdown-picker";
+import firebase from "firebase/compat/app";
+import { getDatabase, ref, onValue,set } from "firebase/database";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
+import * as DocumentPicker from "expo-document-picker";
+import { getStorage, ref as aref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+var uid = "";
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+export default class CreateSong extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      previewImage: "image_1",
+      dropdownHeight: 40,
+      audio: "",
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  async addStory() {
+    if (
+      this.state.title &&
+      this.state.description &&
+      this.state.story &&
+      this.state.moral
+    ) {
+      var d = new Date();
+      let storyData = {
+        preview_image: this.state.previewImage,
+        title: this.state.title,
+        description: this.state.description,
+        story: this.state.story,
+        moral: this.state.moral,
+        created_on: d.toString(),
+        author_uid: uid,
+        likes: 0,
+   
+      };
+      console.log(storyData);
+
+      const db = getDatabase();
+      set(ref(db, "/posts/" + Math.random().toString(36).slice(2)), {
+        storyData,
+      }).then(function (snapshot) {});
+      this.props.setUpdateToTrue();
+      this.props.navigation.navigate("Feed");
+    } else {
+      Alert.alert(
+        "Error",
+        "All fields are required!",
+        [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+        { cancelable: false }
+      );
+    }
+  }
+  handleDocumentSelection = async () => {
+    const result = await DocumentPicker.getDocumentAsync({});
+    if (result.type === "success") {
+      console.log(result.uri);
+      this.setState({ audio: result.uri });
+      this.uploadSong(this.state.audio)
+
+    }
+  };
+  uploadSong = async (uri,) => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("network failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+    try {
+      const storage = getStorage();
+      const storageRef = aref(storage, `songs/song-${uid}`);
+      uploadBytes(storageRef, blob).then((snapshot) => {
+        console.log("uploadbytes")
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      let preview_images = {
+        image_1: require("../assets/music_image_1.jpg"),
+        image_2: require("../assets/music_image_2.jpg"),
+        image_3: require("../assets/music_image_3.jpg"),
+        image_4: require("../assets/music_image_4.png"),
+        image_5: require("../assets/music_image_5.png"),
+      };
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/music-logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>New Story</Text>
+            </View>
+          </View>
+          <View style={styles.fieldsContainer}>
+            <Image
+              source={preview_images[this.state.previewImage]}
+              style={styles.previewImage}
+            ></Image>
+            <View style={{ height: RFValue(this.state.dropdownHeight) }}>
+              <DropDownPicker
+                items={[
+                  { label: "Image 1", value: "image_1" },
+                  { label: "Image 2", value: "image_2" },
+                  { label: "Image 3", value: "image_3" },
+                  { label: "Image 4", value: "image_4" },
+                  { label: "Image 5", value: "image_5" },
+                ]}
+                defaultValue={this.state.previewImage}
+                open={this.state.dropdownHeight == 170 ? true : false}
+                onOpen={() => {
+                  this.setState({ dropdownHeight: 170 });
+                }}
+                onClose={() => {
+                  this.setState({ dropdownHeight: 40 });
+                }}
+                style={{
+                  backgroundColor: "transparent",
+                  borderWidth: 1,
+                  borderColor: "white",
+                }}
+                textStyle={{
+                  color: this.state.dropdownHeight == 170 ? "black" : "white",
+                  fontFamily: "Bubblegum-Sans",
+                }}
+                onSelectItem={(item) =>
+                  this.setState({
+                    previewImage: item.value,
+                  })
+                }
+              />
+            </View>
+            <ScrollView>
+              <TextInput
+                style={styles.inputFont}
+                onChangeText={(title) => this.setState({ title })}
+                placeholder={"Title"}
+                placeholderTextColor="white"
+              />
+
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+                onChangeText={(description) => this.setState({ description })}
+                placeholder={"Description"}
+                multiline={true}
+                numberOfLines={4}
+                placeholderTextColor="white"
+              />
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+                onChangeText={(story) => this.setState({ story })}
+                placeholder={"Story"}
+                multiline={true}
+                numberOfLines={20}
+                placeholderTextColor="white"
+              />
+
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+                onChangeText={(moral) => this.setState({ moral })}
+                placeholder={"Moral of the story"}
+                multiline={true}
+                numberOfLines={4}
+                placeholderTextColor="white"
+              />
+              <View style={styles.submitButton}>
+                <Button
+                  title="upload audio 📑"
+                  onPress={() => this.handleDocumentSelection()}
+                />
+                <Button
+                  onPress={() => this.addStory()}
+                  title="Submit"
+                  color="#841584"
+                />
+              </View>
+            </ScrollView>
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+  },
+  fieldsContainer: {
+    flex: 0.85,
+  },
+  previewImage: {
+    width: "93%",
+    height: RFValue(250),
+    alignSelf: "center",
+    borderRadius: RFValue(10),
+    marginVertical: RFValue(10),
+    resizeMode: "contain",
+  },
+  inputFont: {
+    height: RFValue(40),
+    marginTop: RFValue(40),
+    borderColor: "white",
+    borderWidth: RFValue(1),
+    borderRadius: RFValue(10),
+    paddingLeft: RFValue(10),
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+  },
+  inputFontExtra: {
+    marginTop: RFValue(15),
+  },
+  inputTextBig: {
+    textAlignVertical: "top",
+    padding: RFValue(5),
+  },
+  submitButton: {
+    marginTop: RFValue(20),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
